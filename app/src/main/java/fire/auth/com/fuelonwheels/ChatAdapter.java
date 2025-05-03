743652a2fe757b96d62e748cf5d5cd46daf7ccbc
Mohamma734 <126301493+Mohamma734@users.noreply.github.com>
2025-05-03 23:07:44 +0300

    Create ChatAdapter.java

---
+++ app/src/main/java/fire/auth/com/fuelonwheels/ChatAdapter.java
@@ -0,0 +1,42 @@
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.MessageViewHolder> {
+    private List<Message> messages;
+
+    public ChatAdapter(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        Message message = messages.get(position);
+        holder.tvMessage.setText(message.getText());
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    static class MessageViewHolder extends RecyclerView.ViewHolder {
+        TextView tvMessage;
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvMessage = itemView.findViewById(R.id.tvMessage);
+        }
+    }
+}
