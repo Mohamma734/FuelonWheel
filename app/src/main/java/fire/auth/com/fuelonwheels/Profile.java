169bac2698b4ad7037312399d5cddba1e1f8502a
Mohamma734 <126301493+Mohamma734@users.noreply.github.com>
2025-05-04 18:26:34 +0300

    Update Profile.java

--- app/src/main/java/fire/auth/com/fuelonwheels/Profile.java
+++ app/src/main/java/fire/auth/com/fuelonwheels/Profile.java
@@ -1,7 +1,7 @@
 package fire.auth.com.fuelonwheels;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ProgressBar;
 import android.widget.TextView;
