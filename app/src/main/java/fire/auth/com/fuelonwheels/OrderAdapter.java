33e78a23579c5e8222d9bd7320c969174d1fa808
Mohamma734 <126301493+Mohamma734@users.noreply.github.com>
2025-05-04 18:25:10 +0300

    Update OrderAdapter.java

--- app/src/main/java/fire/auth/com/fuelonwheels/OrderAdapter.java
+++ app/src/main/java/fire/auth/com/fuelonwheels/OrderAdapter.java
@@ -1,12 +1,11 @@
 package fire.auth.com.fuelonwheels;
 
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import java.util.List;
 
 public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
